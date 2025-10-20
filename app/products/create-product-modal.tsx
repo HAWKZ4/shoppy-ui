5429fef37d3bde0c06649295e0ab0e9b3e8ec8cb
M-A <108879264+HAWKZ4@users.noreply.github.com>
2025-10-20 19:36:26 +0300

    feat(product-modal): add product modal and server action
    - Implement create product functionality with modal and action
    - Add floating action button to open modal
    - Rename formError to FormResponse for more generic use and update imports

---
+++ app/products/create-product-modal.tsx
@@ -0,0 +1,82 @@
+"use client";
+
+import { Box, Button, Modal, Stack, TextField } from "@mui/material";
+import { useState } from "react";
+import { FormResponse } from "../common/interfaces/form-response.interface";
+import createProduct from "./create-product";
+
+interface CreateProductModalProps {
+  open: boolean;
+  handleClose: () => void;
+}
+
+export default function CreateProductModal({
+  open,
+  handleClose,
+}: CreateProductModalProps) {
+  const style = {
+    position: "absolute",
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
+    width: 400,
+    bgcolor: "background.paper",
+    border: "2px solid #000",
+    boxShadow: 24,
+    p: 4,
+  };
+
+  const [response, setResponse] = useState<FormResponse>();
+
+  const onClose = () => {
+    setResponse(undefined);
+    handleClose();
+  };
+
+  return (
+    <Modal open={open} onClose={onClose}>
+      <Box sx={style}>
+        <form
+          className="w-full max-w-xs"
+          action={async (formData) => {
+            const response = await createProduct(formData);
+            setResponse(response);
+            if (!response.error) {
+              onClose();
+            }
+          }}
+        >
+          <Stack spacing={2}>
+            <TextField
+              name="name"
+              label="Name"
+              variant="outlined"
+              helperText={response?.error}
+              error={!!response?.error}
+              required
+            />
+            <TextField
+              name="description"
+              label="Description"
+              variant="outlined"
+              helperText={response?.error}
+              error={!!response?.error}
+              required
+            />
+            <TextField
+              name="price"
+              label="Price"
+              variant="outlined"
+              helperText={response?.error}
+              error={!!response?.error}
+              required
+            />
+            <Button type="submit" variant="contained">
+              Submit
+            </Button>
+          </Stack>
+        </form>
+      </Box>
+    </Modal>
+  );
+}
