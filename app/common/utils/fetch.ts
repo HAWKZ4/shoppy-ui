1d9434011225819a5b69cf205a6e7c95d3be2c0c
M-A <108879264+HAWKZ4@users.noreply.github.com>
2025-10-22 13:40:24 +0300

    feat(products): add image upload functionality and save file to server

--- app/common/utils/fetch.ts
+++ app/common/utils/fetch.ts
@@ -2,7 +2,7 @@ import { cookies } from "next/headers";
 import { API_URL } from "../constants/api";
 import { getErrorMessage } from "./errors";
 
-const getHeaders = async () => {
+export const getHeaders = async () => {
   const cookieStore = (await cookies()).toString();
 
   return {
@@ -21,7 +21,7 @@ export const post = async (path: string, formData: FormData) => {
   if (!res.ok) {
     return { error: getErrorMessage(parsedRes) };
   }
-  return { error: "" };
+  return { error: "", data: parsedRes };
 };
 
 export const get = async <T>(path: string, tags?: string[]) => {
--- app/products/create-product/create-product-modal.tsx
+++ app/products/create-product/create-product-modal.tsx
@@ -1,9 +1,17 @@
 "use client";
 
-import { Box, Button, Modal, Stack, TextField } from "@mui/material";
-import { useState } from "react";
+import {
+  Box,
+  Button,
+  Modal,
+  Stack,
+  TextField,
+  Typography,
+} from "@mui/material";
+import { CSSProperties, useState } from "react";
 import { FormResponse } from "../../common/interfaces/form-response.interface";
 import createProduct from "../actions/create-product";
+import CloudUploadIcon from "@mui/icons-material/CloudUpload";
 
 interface CreateProductModalProps {
   open: boolean;
@@ -26,11 +34,25 @@ export default function CreateProductModal({
     p: 4,
   };
 
+  const fileInputStyles: CSSProperties = {
+    clip: "rect(0 0 0 0)",
+    clipPath: "inset(50%)",
+    height: 1,
+    overflow: "hidden",
+    position: "absolute",
+    bottom: 0,
+    left: 0,
+    whiteSpace: "nowrap",
+    width: 1,
+  };
+
   const [response, setResponse] = useState<FormResponse>();
+  const [fileName, setFileName] = useState("");
 
   const onClose = () => {
     setResponse(undefined);
     handleClose();
+    setFileName("");
   };
 
   return (
@@ -71,6 +93,22 @@ export default function CreateProductModal({
               error={!!response?.error}
               required
             />
+            <Button
+              component="label"
+              variant="outlined"
+              startIcon={<CloudUploadIcon />}
+            >
+              Upload File
+              <input
+                type="file"
+                name="image"
+                style={fileInputStyles}
+                onChange={(e) => {
+                  e.target.files && setFileName(e.target.files[0].name);
+                }}
+              />
+            </Button>
+            <Typography>{fileName}</Typography>
             <Button type="submit" variant="contained">
               Submit
             </Button>
